ec125469ea4dab2395a22ee6d5b244d81406a727
machoc <luiscastaing06@hotmail.com>
2017-01-19 14:17:00 -0600

    servidor1

--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -3,10 +3,25 @@ package servidor;
 
 import java.net.ServerSocket;
 import java.util.ArrayList;
+import java.util.Observer;
 import modelo.Modelo;
 
 
 public class Servidor {
+    
+    public Servidor(Modelo nuevoModelo) {
+        datos = nuevoModelo;
+        clientes = new ArrayList<>();
+        numClientes = 0;
+    }
+
+    public Servidor() {
+        this(new Modelo());
+    }
+    
+    public void registrar(Observer nuevoObservador) {
+        datos.addObserver(nuevoObservador);
+    }
     
     //Atributos
     private Modelo datos;
