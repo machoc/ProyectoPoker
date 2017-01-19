30853a002842e8ad2d1240c9223d35d7dd78ed85
machoc <luiscastaing06@hotmail.com>
2017-01-19 14:28:19 -0600

    arreglos servidor

--- Servidor/src/servidor/GestorClientes.java
+++ Servidor/src/servidor/GestorClientes.java
@@ -1,7 +1,50 @@
 
 package servidor;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 
-public class GestorClientes {
+
+public class GestorClientes implements Observer,Runnable {
+    
+    public GestorClientes(Servidor nuevoGestor, Socket skt, int num) {
+        gestorPrincipal = nuevoGestor;
+        direccionCliente = skt.getInetAddress();
+        nCliente = num;
+        nEvento = 0;
+        salida = null;
+        entrada = null;
+        this.socket = skt;
+        try {
+            salida = new ObjectOutputStream(socket.getOutputStream());
+            entrada  = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //Atributos
+    private Servidor gestorPrincipal;
+    private InetAddress direccionCliente;
+    private ObjectOutputStream salida;
+    private ObjectInputStream entrada;
+    private Socket socket;
+    private int nCliente;
+    private int nEvento;
     
 }
--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -2,9 +2,13 @@
 package servidor;
 
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Observer;
+import javax.swing.JOptionPane;
 import modelo.Modelo;
+import protocolo.Protocolo;
 
 
 public class Servidor {
@@ -19,8 +23,91 @@ public class Servidor {
         this(new Modelo());
     }
     
+    public void iniciar(){
+        hiloAtencion = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        });
+        hiloAtencion.start();
+    }
+    
     public void registrar(Observer nuevoObservador) {
         datos.addObserver(nuevoObservador);
+    }
+    
+     public void atenderClientes() {        
+        System.out.println("Atendiendo clientes..");
+        try {
+            srv = new ServerSocket(Protocolo.PUERTO_POR_DEFECTO);
+            srv.setSoTimeout(1000);
+        
+            while (numClientes < Modelo.MAX_JUGADORES && hiloAtencion == Thread.currentThread()) {
+                try {
+                    Socket skt = srv.accept();
+                    numClientes++;
+                    GestorClientes nuevoCliente = new GestorClientes(this, skt,numClientes);
+                    clientes.add(nuevoCliente);           
+                    System.out.println("Agregando: " + nuevoCliente + "Cliente" + numClientes);
+                    registrar(nuevoCliente);
+                    Thread hiloCliente= new  Thread  (nuevoCliente);
+                    hiloCliente.start();
+                    
+                        
+                } catch (SocketTimeoutException e) {
+                    // No se ha conectado ningún cliente.
+                    // Se mantiene esperando conexiones..
+                    
+                }             
+            }
+            JOptionPane.showMessageDialog(null,"El máximo de jugadores es 3");
+            while (true)  { 
+                controlarTurnos(); 
+            }
+        } catch (Exception e) {            
+            System.err.println(e.getMessage());
+        }
+    }
+     
+     public void controlarTurnos(){
+         
+     }
+     
+      public void eliminarClientes() {
+        for(GestorClientes cliente: clientes){
+           clientes.remove(cliente);
+           datos.deleteObserver(cliente);
+        }
+    }
+    
+    public void retirarCliente(GestorClientes cliente) {
+        clientes.remove(cliente);
+        datos.deleteObserver(cliente);
+        System.out.println("Cliente eliminado: " + cliente);
+    }
+    
+    public void cerrarServidor(){            
+           try{
+               srv.close();
+               for(GestorClientes cliente: clientes){
+                   if(cliente!=null){
+                       //cliente.cerrarGestorCliente();
+                   }
+                }
+                
+           }
+           catch (Exception ex)
+           {   
+               System.err.println(ex.getMessage());
+           }
+    }
+    
+    public static Servidor obtenerInstancia(){
+        if(instancia==null){
+            instancia = new Servidor();
+        }
+        return instancia;
     }
     
     //Atributos
