60e8c605827365b8373754777dc061686ca719aa
machoc <luiscastaing06@hotmail.com>
2017-01-19 14:39:48 -0600

    iniciar servidor

--- Servidor/src/servidor/Aplicacion.java
+++ Servidor/src/servidor/Aplicacion.java
@@ -1,12 +1,29 @@
 
 package servidor;
 
+import javax.swing.SwingUtilities;
+
 
 public class Aplicacion {
 
     
     public static void main(String[] args) {
-        
+        Aplicacion aplicacion = new Aplicacion();
+        aplicacion.iniciar();
+    }
+    
+    public void iniciar(){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                mostrarInterfaz();
+            }
+        });
+    }
+    
+    public void mostrarInterfaz(){
+        Servidor gestorPrincipal = new Servidor();
+        gestorPrincipal.iniciar();
     }
     
     
